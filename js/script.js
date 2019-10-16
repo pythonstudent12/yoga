f142c968a0233a5d72bc91d3808ac21d4c9b698b
1 <machinegun2@mail.ru>
2019-10-16 11:00:21 +0300

    Homework #10

--- js/script.js
+++ js/script.js
@@ -37,5 +37,64 @@ info.addEventListener ('click', function (event) {
     
 });
 
+//timer
+
+let deadline = '2019-10-17';
+
+function getTimeRemaining (endtime) {
+    let t = Date.parse(endtime) - Date.parse(new Date()),
+    seconds = Math.floor((t/1000) % 60),
+    minutes = Math.floor((t/1000/60) % 60),
+    hours = Math.floor((t/(1000*60*60)));
+
+    return {
+    'total' : t,
+    'hours' : hours,
+    'minutes' : minutes,
+    'seconds' : seconds
+    };
+
+}
+
+function setClock (id, endtime) {
+  let timer = document.getElementById(id),
+  hours = timer.querySelector('.hours'),
+  minutes = timer.querySelector('.minutes'),
+  seconds = timer.querySelector('.seconds'),
+  timeInterval = setInterval(updateClock, 1000);
+
+function updateClock () {
+   let t1 = getTimeRemaining(endtime);
+   if (t1.seconds <= 9) {
+    t1.seconds = '0' + t1.seconds;
+   }
+   if (t1.minutes <= 9) {
+    t1.minutes = '0' + t1.minutes;
+   }
+   if (t1.hours <= 9) {
+    t1.hours = '0' + t1.hours;
+   }
+   hours.textContent = t1.hours;
+   minutes.textContent = t1.minutes;
+   seconds.textContent = t1.seconds;
+
+   if (t1.total <= 0 ) {
+       clearInterval (timeInterval);
+       hours.textContent = '00';
+       minutes.textContent = '00';
+       seconds.textContent = '00';
+   }
+
+}
+}
+
+
+
+
+
+
+
+setClock ('timer', deadline);
+
 });
 
